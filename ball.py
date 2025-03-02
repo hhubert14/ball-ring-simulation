241fd3c8bd93901fee280abf669d8d491f22352f
hhubert14 <h.hubert@wustl.edu>
2025-03-02 15:38:55 -0600

    Enhance ball simulation with gravity and collision detection; add constants and update ball class

--- ball.py
+++ ball.py
@@ -1,8 +1,58 @@
 import pygame
+import random
+from constants import *
+import math
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 class Ball:
-    def __init__(self, xv=0.1, yv=0.1):
+    def __init__(
+        self,
+        x,
+        y,
+        xv=random.uniform(-2, 2),
+        yv=random.uniform(-2, 2),
+        radius=10,
+    ):
+        self.x = x
+        self.y = y
         self.xv = xv
         self.yv = yv
         self.speed = [self.xv, self.yv]
+        self.color = (
+            random.uniform(0, 255),
+            random.uniform(0, 255),
+            random.uniform(0, 255),
+        )
+        self.radius = radius
+    
+    def update(self):
+        # Δy = v_initial × Δt + (1/2) × g × (Δt)²
+        gravity_change = abs(self.yv) * DELTA_T + (1 / 2) * 9.8 * DELTA_T ** 2
+        self.yv += gravity_change
+
+        self.xv *= AIR_RESISTANCE
+        self.yv *= AIR_RESISTANCE
+
+
+        self.x += self.xv
+        self.y += self.yv
+
+    def draw(self):
+        pygame.draw.circle(
+            screen, 
+            self.color, 
+            (int(self.x), int(self.y)), 
+            self.radius,
+        )
+
+    def check_ring_collision(self, ring_center, ring_radius):
+        centers_distance = math.sqrt(
+            (self.x - ring_center[0]) ** 2 + (self.x - ring_center[1]) ** 2
+        )
+
+        # Collision detected
+        if centers_distance + self.radius >= ring_radius:
+            return True
+
         
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import pygame
+from constants import *
+from ball import Ball
 
 pygame.init()
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Ball Ring Simulation")
@@ -17,33 +16,42 @@ ring_center = (400, 300)  # Center position
 ring_radius = 250  # Radius
 ring_width = 3  # Border thickness
 
-ball_obj = pygame.draw.circle(
-    surface=screen, color=RED, center=[100, 100], radius=40)
+# ball_obj = pygame.draw.circle(
+#     surface=screen, color=RED, center=[100, 100], radius=40)
 
-# define speed of ball
-# speed = [X direction speed, Y direction speed]
-speed = [0.1, 1]
+# # speed = [X direction speed (+ right, - left), Y direction speed (+ down, - up)]
+# speed = [1, -2]
+
+ball = Ball(400, 300)
 
 run = True
+clock = pygame.time.Clock()
 while run:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
     screen.fill(BG_COLOR)
-    
+
     pygame.draw.circle(screen, WHITE, ring_center, ring_radius, ring_width)
 
-    ball_obj = ball_obj.move(speed)
+    ball.draw()
+    ball.update()
 
-    # if ball goes out of screen then change direction of movement
-    if ball_obj.left <= 0 or ball_obj.right >= SCREEN_WIDTH:
-        speed[0] = -speed[0]
-    if ball_obj.top <= 0 or ball_obj.bottom >= SCREEN_HEIGHT:
-        speed[1] = -speed[1]
 
-    # draw ball at new centers that are obtained after moving ball_obj
-    pygame.draw.circle(surface=screen, color=RED,
-        center=ball_obj.center, radius=40)
+
+
+    # ball_obj = ball_obj.move(speed)
+
+    # # if ball goes out of screen then change direction of movement
+    # if ball_obj.left <= 0 or ball_obj.right >= SCREEN_WIDTH:
+    #     speed[0] = -speed[0]
+    # if ball_obj.top <= 0 or ball_obj.bottom >= SCREEN_HEIGHT:
+    #     speed[1] = -speed[1]
+
+    # # draw ball at new centers that are obtained after moving ball_obj
+    # pygame.draw.circle(surface=screen, color=RED,
+    #     center=ball_obj.center, radius=40)
 
     pygame.display.update()
 pygame.quit()
